4425aea073117cefbc4f5d6087a08307f957a88a
carlos676767 <carlitobr730@gmail.com>
2024-04-21 14:56:57 -0700

    agora mostra o binario

--- binario/src/main.ts
+++ binario/src/main.ts
@@ -1,19 +1,28 @@
-const textNerea = document.querySelectorAll("textarea") 
-const button = document.querySelectorAll("button")
+const textNerea = document.querySelectorAll("textarea");
+const button = document.querySelectorAll("button");
 
 const converterValores = (): void => {
-  textNerea[0].value === "" ? alert("vazio") : conversaoBinario()
-}
+  textNerea[0].value === "" ? alert("vazio") : conversaoBinario();
+};
+
+const conversaoBinario = (): void => {
+  const transformarEmArray: string[] = Array.from(textNerea[0].value);
+  const armazenarBinario: string[] = [];
+  for (const pos in transformarEmArray) {
+    const ascTabela: number = transformarEmArray[pos].charCodeAt(0);
+    const transformarEmBinarioInput: string = ascTabela.toString(2);
+    armazenarBinario.push(transformarEmBinarioInput);
+  }
+  mostrarBinario(armazenarBinario)
+};
+
+const mostrarBinario = (binario: string): void => {
+  textNerea[1].innerHTML = binario;
+};
 
 
-const conversaoBinario = () : void => {
-  const transformarEmArray: string[] =  Array.from(textNerea[0].value)
-  const armazenarBi
-  console.log(transformarEmArray);
-  
-}
 
 
-button[0].addEventListener("click", () =>{
-  converterValores()
-})
+button[0].addEventListener("click", () => {
+  converterValores();
+});
